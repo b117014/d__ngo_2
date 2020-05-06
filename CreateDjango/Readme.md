6f1c3e8c8fb43002b59e9ea6b094f8df333a4bc3
b117014 <b117014@iiit-bh.ac.in>
2020-05-07 03:03:16 +0530

    some migration error

--- CreateDjango/Readme.md
+++ CreateDjango/Readme.md
@@ -6,6 +6,7 @@
 2. python3 manage.py startapp <appName>
 
 3. python3 manage.py makemigrations
+     - run this command whenever change into models
      - It looks the model of the app and automatc generate migration in order to change in databases.
      - It's just for models of database whenever adding the database you must have to run this command.
      - used to changes and create datatbase
--- Flights/models.py
+++ Flights/models.py
@@ -2,18 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Airports(models.Model):
-    code = models.CharField(max_length=3)
-    city = models.CharField(max_length=64)
-
-    def __str__(self):
-        return f"code: {self.code} and city: {self.city}"
+# class Airports(models.Model):
+#     code = models.CharField(max_length=5)
+#     city = models.CharField(max_length=64)
+#
+#     def __str__(self):
+#         return f"code: {self.code} and city: {self.city}"
 
 class Flights(models.Model):
-    origin = models.ForeignKey(Airports, on_delete=models.CASCADE, related_name="departure")
+    origin = models.CharField(max_length=64)
     # models.CASCADE for maintain the refferential Integrity
-    destination = models.ForeignKey(Airports, on_delete=models.CASCADE, related_name="arrival")
-
+    destination = models.CharField(max_length=64)
     duration = models.IntegerField()
 
     # whenevr call to fetching the data or String Representation
